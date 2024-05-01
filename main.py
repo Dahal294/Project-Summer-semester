8135ae4cafa6ab2ddb41a474f3aedba47b7489d2
darpan <darpan.dahal@st.ovgu.de>
2024-05-01 12:12:27 +0200

    Second commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+from request import get_result
+
+# url = 'https://en.wikipedia.org/wiki/Springer_Science%2BBusiness_Media'
+
+# response = requests.get(url)
+
+# soup = BeautifulSoup(response.text, "html.parser")
+# print(soup.get_text())
+# print(soup.prettify())
+
+get_result()
