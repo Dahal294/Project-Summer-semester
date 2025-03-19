961fa14c80bb1759f2cc02d57ba08fb3ede39bc9
Dahal294 <161831997+Dahal294@users.noreply.github.com>
2025-03-19 11:41:13 +0100

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -1,29 +1,25 @@
-import gensim
 import nltk
 import numpy as np
 import string
+import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from sklearn.base import BaseEstimator, TransformerMixin
+
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('stopwords')
-import re
 
-
-
-
+# ===================== Preprocessing Document =============================
 class preprocess_document(BaseEstimator, TransformerMixin):
-
-    def fit(self, X, y= None):
+    def fit(self, X, y=None):
         return self
     
     def transform(self, X):
- 
         processed_documents = []
         for document in X["topics"]:
-            document = re.sub(r'\[.*?\]', '', document)
-            document = re.sub(r'[^a-zA-Z\s]', '', document)
+            document = re.sub(r'\[(.*?)\]', r'\1', document)  
+            document = re.sub(r'[^a-zA-Z\s]', '', document)  
             tokens = word_tokenize(document)
             tokens = [word.lower() for word in tokens]
             tokens = [word for word in tokens if word.isalpha()]
@@ -33,15 +29,15 @@ class preprocess_document(BaseEstimator, TransformerMixin):
     
         X["tokens"] = processed_documents
         return X
+# =========================================================================
 
-
+# ===================== Converting Document to Vector =====================
 class document_to_vector(BaseEstimator, TransformerMixin):
-
     def __init__(self, model):
-        super().__init__
+        super().__init__()
         self.model = model
 
-    def fit(self, X, y =None):
+    def fit(self, X, y=None):
         return self
     
     def transform(self, X): 
@@ -50,12 +46,14 @@ class document_to_vector(BaseEstimator, TransformerMixin):
             word_vectors = []
             for token in tokens:
                 if token in self.model:
-                    word_vectors.append(self.model[token])
+                    word_vectors.append(self.model[token.lower()])
             if word_vectors: 
-                document_vector = np.mean(word_vectors, axis = 0)
+                document_vector = np.mean(word_vectors, axis=0)
             else:
                 document_vector = np.zeros(self.model.vector_size)
             document_vectors.append(document_vector)
 
         return np.array(document_vectors)
+# =========================================================================
+
 
