2ceeb96fc111f2dffa18c3d8bd763f4c0ccdd24e
Dahal294 <161831997+Dahal294@users.noreply.github.com>
2025-03-19 11:43:00 +0100

    Update topic.py

--- topic.py
+++ topic.py
@@ -1,14 +1,18 @@
 import gensim
 
-
-def generate_topic( pipeline, dataframe):
+# ===================== Generate Topic =====================
+def generate_topic(pipeline, dataframe):
     processed_text = pipeline.named_steps["preprocess"].transform(dataframe)
-    dictionary = gensim.corpora.Dictionary(processed_text["tokens"])
-    corpus = [dictionary.doc2bow(text) for text in processed_text['tokens']]
+    tokens = processed_text["tokens"]
+    dictionary = gensim.corpora.Dictionary(tokens)
+    corpus = [dictionary.doc2bow(text) for text in tokens]
+    
+    if not any(corpus):
+        return "No topic"
+    
     ldamodel = gensim.models.LdaModel(corpus, num_topics=1, id2word=dictionary)
     top_words = [dictionary[word_id] for word_id, prob in ldamodel.get_topic_terms(0, topn=2)]
     
-    # Return the words as a string joined by " + "
     return '  '.join(top_words)
-
+# ===========================================================
 
